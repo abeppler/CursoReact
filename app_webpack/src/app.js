65f07252cd3a8d2456be6d7f8f701cbfca37507a
Anderson Beppler <abeppler2000@gmail.com>
2017-04-19 20:10:13 -0300

    evento

--- app_webpack/src/app.js
+++ app_webpack/src/app.js
@@ -4,6 +4,9 @@ import Title from './title'
 const App = () => (
     <div>
         <Title valor="Bosta"/>
+        <button onClick={() => console.log('clicou')}>
+            Clique em mim!
+        </button>
     </div>
 )
 
